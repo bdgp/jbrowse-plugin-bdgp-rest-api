2b35fe2a07b41b7d3547718af117901bf461b688
Ben Booth <benwbooth@gmail.com>
2021-10-15 23:40:01 +0000

    finish initial writeup

---
+++ src/BDGPAdapter/index.js
@@ -0,0 +1,95 @@
+import {
+  ConfigurationSchema,
+  readConfObject,
+} from "@jbrowse/core/configuration";
+import { ObservableCreate } from "@jbrowse/core/util/rxjs";
+import { BaseFeatureDataAdapter } from "@jbrowse/core/data_adapters/BaseAdapter";
+import SimpleFeature from "@jbrowse/core/util/simpleFeature";
+import stringify from "json-stable-stringify";
+
+export const configSchema = ConfigurationSchema(
+  http_"BDGPAdapter",
+  {
+    source: {
+      type: "string",
+      description: "the track source",
+      defaultValue: "",
+    },
+    track: {
+      type: "string",
+      description: "the track to select data from",
+      defaultValue: "",
+    },
+    prefix: {
+      type: "string",
+      description: "url prefix to use for name searches",
+      defaultValue: "",
+    },
+  },
+  { explicitlyTyped: true },
+);
+
+export class AdapterClass extends BaseFeatureDataAdapter implements BaseTextSearchAdapter {
+  constructor(config) {
+    super(config);
+    this.config = config;
+  }
+
+  getFeatures(region) {
+    const { assemblyName, start, end, refName } = region;
+    return ObservableCreate(async observer => {
+      const source = readConfObject(this.config, "source");
+      const track = readConfObject(this.config, "track");
+      const prefix = readConfObject(this.config, "prefix");
+      try {
+        if (source === 'labtrack') {
+          const result = await fetch(
+            `/cgi-bin/labtrack/query/data/jbrowse.pl/track/${encodeURIComponent(track)}/features/${encodeURIComponent(refName)}?start=${encodeURIComponent(start)}&end=${encodeURIComponent(end)}`
+          );
+          if (!result.ok) {
+            throw new Error(
+              `Failed to fetch ${result.status} ${result.statusText}`,
+            );
+          }
+          const data = await result.json();
+          for (feature of data['features']) {
+            observer.next(new SimpleFeature({
+              id: feature.uniqueID,
+              data: {
+                ...feature,
+                refName
+              },
+            }));
+          }
+          observer.complete();
+        }
+        else {
+          throw new Error(`Unknown source parameter: ${source}`);
+        }
+      } catch (e) {
+        observer.error(e);
+      }
+    });
+  }
+
+  async searchIndex(args: BaseArgs, searchScope: SearchScope) {
+    const prefix = readConfObject(this.config, "prefix");
+    const result = await fetch(
+            `${prefix}/jbrowse/${encodeURIComponent(searchScope.assemblyName)}/name?${args.searchType==='prefix'?'startswith':'equals'}=${encodeURIComponent(args.queryString)}`
+    );
+    if (!result.ok) {
+      throw new Error(
+        `Failed to fetch ${result.status} ${result.statusText}`,
+      );
+    }
+    const results = await result.json();
+    const locstrings = results.map(entry => {
+      return new LocStringResult({
+        locString: `${entry.location.ref}:${entry.location.start}-${entry.location.end}`,
+        label: entry.name,
+      })
+    }).slice(0, args.limit? args.limit : undefined);
+    return locstrings;
+  }
+  freeResources() {}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import AdapterType from "@jbrowse/core/pluggableElementTypes/AdapterType";
 import Plugin from "@jbrowse/core/Plugin";
-import { AdapterClass, configSchema } from "./UCSCAdapter";
+import { AdapterClass, configSchema } from "./BDGPAdapter";
 import { version } from "../package.json";
 
-export default class UCSCPlugin extends Plugin {
-  name = "UCSCPlugin";
+export default class BDGPPlugin extends Plugin {
+  name = "BDGPPlugin";
   version = version;
   install(pluginManager: any) {
     pluginManager.addAdapterType(
       () =>
         new AdapterType({
-          name: "UCSCAdapter",
+          name: "BDGPAdapter",
           configSchema,
           AdapterClass,
         }),
